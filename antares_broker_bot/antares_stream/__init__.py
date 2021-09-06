9f357778b92460141fd9a3231e4d8ff911e70cd6
Konstantin Malanchev <hombit@gmail.com>
2021-09-06 18:15:03 +0300

    Typo

--- antares_broker_bot/antares_stream/__init__.py
+++ antares_broker_bot/antares_stream/__init__.py
@@ -11,7 +11,7 @@ from antares_broker_bot.config import ANTARES_API_KEY, ANTARES_API_SECRET
 def stream_alerts(topic: str) -> Generator[Locus, None, None]:
     with StreamingClient(topics=[topic], api_key=ANTARES_API_KEY, api_secret=ANTARES_API_SECRET) as client:
         for _topic, locus in client.iter():
-            logging.info(f'Just recieved locus {locus.locus_id}')
+            logging.info(f'Just received locus {locus.locus_id}')
             yield locus
 
 
