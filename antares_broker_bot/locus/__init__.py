9986fcad139422fc2c65c2f5470818016679f9de
Konstantin Malanchev <hombit@gmail.com>
2021-09-17 10:52:35 +0200

    Better message formatting

--- antares_broker_bot/locus/__init__.py
+++ antares_broker_bot/locus/__init__.py
@@ -1,3 +1,4 @@
+from aiogram.utils.markdown import escape_md
 from antares_client._api.models import Locus
 from prettytable import PrettyTable
 
@@ -10,15 +11,16 @@ async def compose_message_from_locus(locus: Locus) -> str:
     table.add_column('', ['amplitude', 'mean'])
     for band in BANDS:
         table.add_column(
-            f'**{band}**',
+            f'{band}',
             [
                 locus.properties[f'feature_amplitude_magn_{band}'],
                 locus.properties[f'feature_weighted_mean_magn_{band}'],
             ],
         )
+    table.float_format = '.2'
+    table_str = escape_md(table.get_string())
 
-    return f'''**{locus.locus_id}**
-    ```text
-    {table.get_string(float_format='.2')}
-    ```
-    https://antares.noirlab.edu/loci/{locus.locus_id}'''
+    return rf'''https://antares\.noirlab\.edu/loci/{locus.locus_id}
+```text
+{table_str}
+```'''
